ea9da87069cb51916dc074d88795ab1a79881a93
Sotiris Photiadis <fotiadissotiris0@gmail.com>
2025-11-20 02:50:18 +0200

    changed the whole file in order to make the code work

--- main.py
+++ main.py
@@ -14,25 +14,43 @@ from PostProcessor import plot_mesh, plot_mesh_interactive, plot_temperature_fie
 from PostProcessor import export_temperature_csv
 
 
-# Import model info
-nodes, elems, materials, k, bcs = read_input_file('validation.semfe')
+def main():
+    # 1. Διάβασμα του .feb / .semfe αρχείου
+    feb_file = "validation.semfe"   # ή όποιο όνομα έχεις
+    nodes, elems, materials, k, bcs = read_input_file(feb_file)
 
-# Check Mesh Quality
-plot_mesh_interactive(nodes, elems, show=True, filename='interactive_mesh2.html')
+    # 2. Συναρμολόγηση global K και f
+    K = assemble_global(nodes, elems, k)
+    f = np.zeros(nodes.shape[0])
 
-# Assemble global
-K = assemble_global(nodes, elems, k=k)
+    # 3. Εφαρμογή Neumann (heat flux) BCs
+    #    bcs['heat_flux'] είναι λίστα (elem_id, edge_id, q)
+    if bcs.get("heat_flux"):
+        f = apply_heat_flux(f, nodes, elems, bcs["heat_flux"])
 
-# Apply BCs
-bc_nodes = [node for node, val in bcs['temperature']]
-bc_values = [val for node, val in bcs['temperature']]
-Kmod, fmod = apply_dirichlet(K, np.zeros(nodes.shape[0]), bc_nodes, bc_values)
-fmod       = apply_heat_flux(fmod, nodes, elems, bc_values)
-Kmod, fmod = apply_convection(K, fmod, nodes, elems, bc_values)
+    # 4. Εφαρμογή Convection (Robin) BCs
+    #    bcs['convection'] είναι λίστα (elem_id, edge_id, h, Tinf)
+    if bcs.get("convection"):
+        K, f = apply_convection(K, f, nodes, elems, bcs["convection"])
 
-#
-u = solve_system(Kmod, fmod)
+    # 5. Εφαρμογή Dirichlet (Temperature) BCs
+    #    bcs['temperature'] είναι λίστα (node, value)
+    if bcs.get("temperature"):
+        bc_nodes  = [node for node, val in bcs["temperature"]]
+        bc_values = [val  for node, val in bcs["temperature"]]
+        K, f = apply_dirichlet(K, f, bc_nodes, bc_values)
 
-# Call it in main
-plot_temperature_field(nodes, elems, u, filename='temperature_field.png')
-export_temperature_csv(nodes, u)
+    # 6. Λύση συστήματος
+    u = solve_system(K, f)
+
+    # 7. Post-processing
+    #    (αν θες, ξεσχολιάζεις και το plot_mesh / plot_mesh_interactive)
+    plot_mesh(nodes, elems, filename="mesh.png")
+    plot_mesh_interactive(nodes, elems)
+
+    plot_temperature_field(nodes, elems, u, filename="temperature_field.png")
+    export_temperature_csv(nodes, u)
+
+
+if __name__ == "__main__":
+    main()
